d9f2dcac3459ae5170d51e239f810a76ea2396fa
= <vanshbhoyar7105@gmail.com>
2026-09-23 19:04:09 +0530

    commit changes

--- src/main/java/com/lcwd/electronicStore/config/SecurityConfig.java
+++ src/main/java/com/lcwd/electronicStore/config/SecurityConfig.java
@@ -4,12 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -35,6 +38,16 @@ public class SecurityConfig {
 ////        UserDetailsService IS INTERFACE SO we use InMemoryUserDetailsManager bcz it is implementation
 //        return new InMemoryUserDetailsManager(admin,normal);
 //    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(csrf->csrf.disable())
+                .cors(cors->cors.disable())
+                .authorizeHttpRequests(auth->auth.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
 
     @Bean
     public DaoAuthenticationProvider authenticationProvider(){
